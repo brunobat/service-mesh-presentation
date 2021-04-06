35fb3992475c58105e736931654aaaaf91b8273f
Bruno Baptista <bruno.baptista@talkdesk.com>
2021-04-06 09:57:48 +0100

    propagate trace

--- apps/hero-service/src/main/java/org/acme/hero/camel/SuperHeroCreator.java
+++ apps/hero-service/src/main/java/org/acme/hero/camel/SuperHeroCreator.java
@@ -1,5 +1,9 @@
 package org.acme.hero.camel;
 
+import io.opentracing.SpanContext;
+import io.opentracing.Tracer;
+import io.opentracing.propagation.Format;
+import io.opentracing.propagation.TextMapExtractAdapter;
 import org.acme.hero.model.CapeType;
 import org.acme.hero.model.Hero;
 import org.apache.camel.Exchange;
@@ -12,6 +16,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.Map;
 
 import static javax.transaction.Transactional.TxType.REQUIRED;
 
@@ -24,20 +29,36 @@ public class SuperHeroCreator implements Processor {
     @Inject
     private EntityManager entityManager;
 
+    @Inject
+    Tracer tracer;
+
     @Override
     public void process(final Exchange exchange) throws Exception {
+
         final String legumeItem = exchange.getMessage().getBody(String.class);
+
+        SpanContext spanContext = tracer.extract(Format.Builtin.TEXT_MAP,
+                                                 new TextMapExtractAdapter(Map.of(
+                                                     "uber-trace-id",
+                                                     (String) exchange.getMessage().getHeaders().get("uber-trace-id"))
+                                                 ));
+        tracer.buildSpan("addHero")
+              .asChildOf(spanContext)
+              .withTag("legumeItem", legumeItem)
+              .startActive(true);
+
         add(legumeItem);
+
+        tracer.activeSpan().finish();
     }
 
     @Transactional(REQUIRED)
     Hero add(final String legumeItem) {
         log.info("Legume received: {}", legumeItem);
-
         final Hero hero = Hero.builder()
-                .name("SUPER-" + legumeItem)
-                .capeType(CapeType.SUPERMAN)
-                .build();
+                              .name("SUPER-" + legumeItem)
+                              .capeType(CapeType.SUPERMAN)
+                              .build();
 
         final Hero createdHero = entityManager.merge(hero);
         log.info("hero created: {}", createdHero);
--- apps/legume-service/src/main/java/org/acme/legume/camel/MessageSender.java
+++ apps/legume-service/src/main/java/org/acme/legume/camel/MessageSender.java
@@ -1,25 +1,58 @@
 
 package org.acme.legume.camel;
 
+import io.opentracing.Tracer;
+import io.opentracing.propagation.Format;
+import io.opentracing.propagation.TextMap;
+import lombok.extern.slf4j.Slf4j;
 import org.acme.legume.data.LegumeItem;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
+import org.eclipse.microprofile.opentracing.Traced;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
+@Slf4j
 @ApplicationScoped
 public class MessageSender {
 
     @Inject
     private CamelContext context;
 
+    @Inject
+    Tracer tracer;
+
+    @Traced
     public String send(final LegumeItem legumeItem) {
-        final ProducerTemplate template = context.createProducerTemplate();
-        template.sendBody("direct:rabbitMQ", legumeItem.getName().getBytes(UTF_8));
+        try (final ProducerTemplate template = context.createProducerTemplate()) {
+            template.sendBodyAndHeaders("direct:rabbitMQ", legumeItem.getName().getBytes(UTF_8), getTraceContext());
+        } catch (IOException e) {
+            log.info("Error sending message", e);
+        }
         return legumeItem.getName();
+    }
+
+    private Map<String, Object> getTraceContext() {
+        final Map<String, Object> openTracingContext = new HashMap<>();
+
+        tracer.inject(tracer.activeSpan().context(), Format.Builtin.TEXT_MAP, new TextMap() {
+            @Override public Iterator<Map.Entry<String, String>> iterator() {
+                throw new UnsupportedOperationException("TextMapInjectAdapter should only be used with Tracer.inject()");
+            }
+
+            @Override public void put(final String s, final String s1) {
+                openTracingContext.put(s, s1);
+            }
+        });
+
+        return openTracingContext;
     }
 
 }
